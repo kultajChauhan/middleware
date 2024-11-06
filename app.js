860f46850a38fdd1586fa7c489eaef7f873a0e02
kultajSInghChauhan <kultajchauhan121@gmail.com>
2024-11-06 23:41:45 +0530

    Error Class

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 let express=require("express");
 let app=express();
+const ExpressError=require("./ExpressError")
+
+const checkToken= (req,res,next)=>{
+    const {token}=req.query;
+    if(token==="giveaccess"){
+        next()
+    }else{
+        throw new ExpressError(401,"ACCESS DENIDE!!");
+    }
+}
+
+app.get("/api",checkToken,(req,res)=>{
+    res.send("data");
+});
 
 app.use((req,res,next)=>{
 req.restime=new Date(Date.now()).toString();
